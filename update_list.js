90cf6aea392bdf5561a7f094f1a9e589b4df60b2
BenjaminLadick <bladick@asu.edu>
2017-10-21 18:02:14 -0700

    create the config variable and firebase info to access DB and made some edits to the current backend

--- update_list.js
+++ update_list.js
@@ -11,14 +11,33 @@
 function getNewList() {
   // grab data from IN
 
+
   // format for queries
+
 
   // run query/queries
 
+
   // return JSON list
+
+
 }
 
 window.onload = function() {
-  getNewList();
-  document.getElementById("refresh_button").onclick = getNewList;
+  // get the firebase DB
+  var config = {
+      apiKey: "AIzaSyAgd55aG5L_fMmY_RWfSPIFEyKGSu9-_TY",
+      authDomain: "ja-arizona.firebaseapp.com",
+      databaseURL: "https://ja-arizona.firebaseio.com",
+      projectId: "ja-arizona",
+      storageBucket: "ja-arizona.appspot.com",
+      messagingSenderId: "295885839216"
+  };
+  firebase.initializeApp(config);
+
+  newList = getNewList();
+  document.getElementById("refresh_button").onclick = function() {
+    newList = getNewList();
+  };
+  // RUN FUNCTION TO CONVERT TO HTML AND UPDATE ACTUAL LIST
 };
