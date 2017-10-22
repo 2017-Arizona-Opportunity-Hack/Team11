71f4126115a4e883426c9bae85611b1c868d7114
BenjaminLadick <bladick@asu.edu>
2017-10-21 17:53:51 -0700

    Created the JS query getNewList backbone

---
+++ update_list.js
@@ -0,0 +1,24 @@
+/*
+  getNewList():
+    - grab data from IN
+    - format for queries
+    - run query/queries
+    - return JSON list
+
+  onload = refresh.click = getNewList()
+*/
+
+function getNewList() {
+  // grab data from IN
+
+  // format for queries
+
+  // run query/queries
+
+  // return JSON list
+}
+
+window.onload = function() {
+  getNewList();
+  document.getElementById("refresh_button").onclick = getNewList;
+};
